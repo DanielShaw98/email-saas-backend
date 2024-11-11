ed774f51fa386655a044d9d422efc622af720c6a
Daniel Shaw <danielshaw71@gmail.com>
2024-11-11 18:28:46 +0000

    Setup user model, authroutes, authmiddleware and adjusted server file

---
+++ src/models/User.ts
@@ -0,0 +1,53 @@
+import mongoose, { Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      next(error);
+    } else {
+      next(new Error('An unknown error occurred'));
+    }
+  }
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,29 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("Missing JWT_SECRET in environment variables.");
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 connectDB();
+
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).json({ message: 'An internal server error occurred' });
 });
 
 app.listen(port, () => {
